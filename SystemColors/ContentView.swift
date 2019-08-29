c0b507f88a91e42843e24c1e85de1e11bef73b2c
Ricardo Sánchez Sotres <odrakir@gmail.com>
2019-08-29 09:50:41 +0200

    Initial Commit

---
+++ SystemColors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SystemColors
+//
+//  Created by Ricardo Sánchez Sotres on 29/08/2019.
+//  Copyright © 2019 Ricardo Sánchez Sotres. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
